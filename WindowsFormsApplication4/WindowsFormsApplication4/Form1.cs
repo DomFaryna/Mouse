927f08f4e5b939a85d41cc17fa33dbb7c498ac89
Dominic Faryna <dominic.faryna@gmail.com>
2015-11-03 20:03:02 -0500

    Most functionaily is added. Can move mouse, click mouse, and preform keyboard letters

--- WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -8,11 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Runtime.InteropServices;
 
 namespace WindowsFormsApplication4
 {
     public partial class Mouse_Mover : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, int dx, int dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
         public Mouse_Mover()
         {
             InitializeComponent();
@@ -21,6 +28,11 @@ namespace WindowsFormsApplication4
         int[] mouse_pos = new int[20]; // will keep track of a
         int i = 0;
         int j = 0;
+        int k = 0;
+        int l = 0;
+        int num = 0;
+        int numb = 0;
+        String to = "Lala";
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -40,17 +52,17 @@ namespace WindowsFormsApplication4
 
             if (e.KeyCode == Keys.B)
             {
-               
+                
             }
 
             if (swag == 1)
             {
-                timer1.Start();
+               // timer1.Start();
             }
 
             if (swag == 0)
             {
-                timer1.Stop();
+               // timer1.Stop();
                 
             }
             if (swag == 1 && e.KeyCode == Keys.A)
@@ -81,7 +93,87 @@ namespace WindowsFormsApplication4
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            SendKeys.Send("^C");
+            if (swag == 2)
+            {
+                
+                start_mouse(numb);
+            }
+        }
+
+        void mouse_cl()
+        {
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            to = textBox1.Text;
+            listView1.Items.Add(to);
+            textBox1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            while(k != i)
+            {
+                mouse_pos[k] = 0;
+                k++;
+            }
+
+            k = 0;
+            i = 0;
+            j = 0;
+
+            listView1.Items.Add("i is cleared");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            swag = 2;
+            listView1.Items.Clear();
+            listView1.Items.Add("Starting Cycle");
+            //Cursor.Position = new Point(mouse_pos[0], mouse_pos[1]);
+            timer1.Start();
+            numb = int.Parse(textBox1.Text);
+            textBox1.Clear();
+            
+
+
+        }
+
+        void start_mouse(int e)
+        {
+            if (num < e) {
+                if (i > l)
+                {
+                    Cursor.Position = new Point(mouse_pos[l], mouse_pos[l + 1]);
+                    l += 2;
+                    listView1.Items.Add("Test");
+                }
+                else
+                {
+                    l = 0;
+                    listView1.Items.Add(num.ToString() + " times");
+                    num++;
+                }
+            }
+            else
+            {
+                swag = 0;
+                listView1.Items.Clear();
+                listView1.Items.Add("All done");
+                timer1.Stop();
+                num = 0;
+            }
+
         }
     }
 }
